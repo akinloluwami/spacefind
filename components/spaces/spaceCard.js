413fd75c976f5bcc56d8d44163c75a7bac99dacf
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-10 03:51:15 +0100

    added props

--- components/spaces/spaceCard.js
+++ components/spaces/spaceCard.js
@@ -1,1 +1,18 @@
 //indivial space on the explore page
+
+import React from "react";
+
+function SpaceCard({
+  title,
+  status,
+  listeners,
+  description,
+  id,
+  host,
+  topic,
+  shedule,
+}) {
+  return <div>SpaceCard</div>;
+}
+
+export default SpaceCard;
