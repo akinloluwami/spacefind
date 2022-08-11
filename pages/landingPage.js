90c2dddd72610d70f314ff3f9511d1394a93f3fb
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-11 01:07:54 +0100

    .

--- pages/landingPage.js
+++ pages/landingPage.js
@@ -1,1 +1,8 @@
 //landing page
+
+//1. Hero text (Discover "what's happening")
+//2. Sub text ("Find The Best Twitter Spaces To Join")
+//3. Hero CTAs (explore, sign in)
+//4. Features (discover, search, get notified, make list of spaces and share)
+//5.Android and iOS app coming soon
+//6. Footer ("Made with (x^2 + y^2 – 1)^3 = x^2 y^3 from Nigeria")
